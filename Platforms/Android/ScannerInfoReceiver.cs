f6a7d16d258fe8425d26b20a20f9ffc265bf4785
1211-0452\admin <vishalb@alohatechnology.com>
2025-10-29 17:41:33 +0530

    c

--- Platforms/Android/ScannerInfoReceiver.cs
+++ Platforms/Android/ScannerInfoReceiver.cs
@@ -7,10 +7,14 @@ using Application = Android.App.Application;
 namespace USB_EMDK.Platforms.Android
 {
     [BroadcastReceiver(Enabled = true)]
-    [IntentFilter(new[] { "com.symbol.datawedge.api.RESULT_ENUMERATE_SCANNERS" })]
+    [IntentFilter(new[] {
+        "com.symbol.datawedge.api.RESULT_ENUMERATE_SCANNERS",
+        "com.symbol.datawedge.api.RESULT_GET_DEVICE_SERIAL_NUMBER"
+    })]
     public class ScannerInfoReceiver : BroadcastReceiver
     {
         public static event Action<string>? OnSerialReceived;
+        public static event Action<List<string>>? OnScannersEnumerated;
 
         public override void OnReceive(Context context, Intent intent)
         {
@@ -19,31 +23,48 @@ namespace USB_EMDK.Platforms.Android
                 var scanners = intent.GetParcelableArrayExtra("com.symbol.datawedge.api.RESULT_ENUMERATE_SCANNERS");
                 if (scanners == null) return;
 
+                var scannerNames = new List<string>();
                 foreach (var s in scanners)
                 {
                     if (s is Bundle b)
                     {
-                        string serial = b.GetString("SERIAL_NUMBER");
-                        if (!string.IsNullOrEmpty(serial))
+                        string scannerName = b.GetString("SCANNER_NAME");
+                        if (!string.IsNullOrEmpty(scannerName))
                         {
-                            OnSerialReceived?.Invoke(serial);
-                            break; // take first connected scanner
+                            scannerNames.Add(scannerName);
                         }
                     }
+                }
+                OnScannersEnumerated?.Invoke(scannerNames);
+            }
+            else if (intent.Action == "com.symbol.datawedge.api.RESULT_GET_DEVICE_SERIAL_NUMBER")
+            {
+                string serial = intent.GetStringExtra("com.symbol.datawedge.api.RESULT_GET_DEVICE_SERIAL_NUMBER");
+                if (!string.IsNullOrEmpty(serial))
+                {
+                    OnSerialReceived?.Invoke(serial);
                 }
             }
         }
     }
+
     public static class ZebraScannerHelper
     {
         public static void RequestScannerSerial()
         {
             var context = Platform.AppContext ?? Application.Context;
+            Intent dwIntent = new Intent();
+            dwIntent.SetAction("com.symbol.datawedge.api.ACTION");
+            dwIntent.PutExtra("com.symbol.datawedge.api.GET_DEVICE_SERIAL_NUMBER", "");
+            context.SendBroadcast(dwIntent);
+        }
 
+        public static void EnumerateScanners()
+        {
+            var context = Platform.AppContext ?? Application.Context;
             Intent dwIntent = new Intent();
             dwIntent.SetAction("com.symbol.datawedge.api.ACTION");
             dwIntent.PutExtra("com.symbol.datawedge.api.ENUMERATE_SCANNERS", "");
-
             context.SendBroadcast(dwIntent);
         }
     }
